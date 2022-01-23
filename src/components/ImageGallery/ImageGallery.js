4f18dd82e786fd8d824ad89611c48a76d39a2c1c
Yaroslav Borys <borrline@gmail.com>
2022-01-23 14:43:45 +0200

    1

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -20,6 +20,7 @@ class ImageGallery extends Component {
     largeImage: "",
     alt: "",
     page: 1,
+    loading: false,
   };
   scrollToBottom = () => {
     window.scrollTo({
@@ -48,7 +49,7 @@ class ImageGallery extends Component {
     const { page } = this.state;
 
     if (prevName !== nextName) {
-      this.setState({ images: [], page: 1 });
+      this.setState({ images: [], page: 1, loading: true });
     }
 
     if ((prevProps !== this.props && page === 1) || prevState.page !== page) {
@@ -57,6 +58,7 @@ class ImageGallery extends Component {
         this.setState({
           images: [...this.state.images, ...images.hits],
           status: "resolved",
+          loading: false,
         })
       );
       if (page !== 1) this.scrollToBottom();
@@ -78,12 +80,15 @@ class ImageGallery extends Component {
   };
 
   render() {
-    const { images, error, status, showModal, alt, largeImage } = this.state;
+    const { images, error, status, showModal, alt, largeImage, loading } =
+      this.state;
+
     <div></div>;
     if (status === "idle") {
       return <div className={s.text}>Введите название </div>;
     }
-    if (status === "pending") {
+
+    if (status === "resolved" || status === "pending") {
       return (
         <div className={s.text}>
           <ul className={s.ImageGallery}>
@@ -97,53 +102,34 @@ class ImageGallery extends Component {
                 largeURL={largeImageURL}
               />
             ))}
-            <Loader />
-            {showModal && (
-              <Modal
-                onClose={this.closeModal}
-                alt={alt}
-                largeImage={largeImage}
-              />
-            )}
           </ul>
+          {showModal && (
+            <Modal
+              onClose={this.closeModal}
+              alt={alt}
+              largeImage={largeImage}
+            />
+          )}
+
+          {images.length > 0 ? (
+            <Button onClick={this.loadNextPage} />
+          ) : (
+            <p className={s.text}>Такого запроса не существует</p>
+          )}
+        </div>
+      );
+    }
+
+    if (loading) {
+      return (
+        <div className={s.text}>
+          <Loader />
         </div>
       );
     }
     if (status === "rejected") {
       return <h1 className={s.text}>{error.message}</h1>;
     }
-    if (images.length === 0) {
-      return <p className={s.text}>Такого запроса не существует</p>;
-    }
-
-    if (status === "resolved") {
-      return (
-        <div className={s.content}>
-          <ul className={s.ImageGallery}>
-            {images.map(({ id, webformatURL, tags, largeImageURL }) => (
-              <ImageGalleryItem
-                onClick={this.openModal}
-                className={s.ImageGalleryItemImag}
-                key={id}
-                url={webformatURL}
-                alt={tags}
-                largeURL={largeImageURL}
-              />
-            ))}
-            {showModal && (
-              <Modal
-                onClose={this.closeModal}
-                alt={alt}
-                largeImage={largeImage}
-              />
-            )}
-          </ul>
-          <Button onClick={this.loadNextPage} />
-        </div>
-      );
-    }
-
-    return null;
   }
 }
 
